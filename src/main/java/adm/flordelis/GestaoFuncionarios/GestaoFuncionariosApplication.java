acada3f8df87386f76bd492671aa223a1644702a
Luiz <luizeduardo2099@gmail.com>
2026-02-02 14:43:57 -0300

    Commit inicial

---
+++ src/main/java/adm/flordelis/GestaoFuncionarios/GestaoFuncionariosApplication.java
@@ -0,0 +1,13 @@
+package adm.flordelis.GestaoFuncionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoFuncionariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/adm/flordelis/GestaoFuncionarios/GestaoFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package adm.flordelis.GestaoFuncionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
